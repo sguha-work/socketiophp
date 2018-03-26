3d6e6cd05d78c3ccad9df091dfe3133ff243da17
sguha-work <sguha1988.life@gmail.com>
2018-03-26 15:40:29 +0530

    fcm working

--- fcm/messeging.php
+++ fcm/messeging.php
@@ -1,28 +1,35 @@
 <?php
 class Messeging {
-    function sendmessage_android($devicetoken,$message){ 
-            $api_key     =   'AIzaSyCtDch9K3ZqGF_SSLYCz4JjMS4-fkJuW';//get the api key from FCM backend
-            $url = 'https://fcm.googleapis.com/fcm/send';
-            $fields = array('registration_ids'  => array($devicetoken));//get the device token from Android 
-            $headers = array( 'Authorization: key=' . $api_key,'Content-Type: application/json');
+    private $deviceToken;
+    private $apiKey = "AAAA3oOSzMM:APA91bEJ9ePaqevVwIHRQQqz7VIgK9PWkFm1BH8V8xpSL6PQ5uwLeT3y1FEq-AXPX6hCPyFHFt3NU22XNXb3kepVSc6hPFVyp0K5sNqqGeNlFwgmbel7A2kUTRw5ojQvw9SPjButszjs";
+    private $url = "https://fcm.googleapis.com/fcm/send";
+
+    function __construct($deviceToken) {
+        $this->deviceToken = $deviceToken;
+    }
+    function sendmessage_android($title, $message){ 
+            $fields = array('registration_ids'  => array($this->deviceToken), 'notification' => array('title' => $title, 'body' => $message));//get the device token from Android 
+            $headers = array( 'Authorization: key=' . $this->apiKey,'Content-Type: application/json');
             $ch = curl_init();
-            curl_setopt( $ch, CURLOPT_URL, $url );
+            curl_setopt( $ch, CURLOPT_URL, $this->url );
             curl_setopt( $ch, CURLOPT_POST, true );
             curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
             curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0); 
             curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields) );
             $result = curl_exec($ch);
             if(curl_errno($ch)){
                 return 'Curl error: ' . curl_error($ch);
             }
             curl_close($ch);
-            $cur_message=json_decode($result);
-            if($cur_message->success==1)
-                return true;
-            else
-                return false;
+            $curl_message=json_decode($result);
+            if(gettype($curl_message)=="object" && $curl_message->success == 1) {
+                return $result;
+            } else {
+                return "{'success':0, 'failure': 1}";
+            }
+            
     }
 }
 ?>
